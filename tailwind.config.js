88df44748d1a23017bde121b28b3600f0a3060ca
Michael Lei <michaelleie0ca@gmail.com>
2023-04-19 02:45:34 -0700

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#bc4123",
+				secondary: "#2b3452",
+			},
+		},
+	},
+	plugins: [],
+};
